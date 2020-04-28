df16c0104cfc2dd2a836056986d369f6a2ebee68
Xueqi Wang <55414437+xueqiwang0v0@users.noreply.github.com>
2020-04-28 01:24:14 -0400

    Update Hangman.java

--- Hangman.java
+++ Hangman.java
@@ -2,171 +2,11 @@ package HangmanGame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;package HangmanGame;
-
-import java.util.Random;
 import java.util.Scanner;
-import java.util.Map.Entry;
-import java.util.Iterator;
-import java.util.Map;
-
-public class HangmanGame {
-	
-	/**
-	 * "mode": true means traditionalHangman, false means evilHangman.
-	 */
-	private boolean mode;
-	
-	/**
-	 * The constructor randomly selects the game mode.
-	 */
-	HangmanGame(){
-		Random r = new Random();
-		// randomly select the mode of hangman game
-		this.mode = r.nextBoolean();
-	}
-	
-	/**
-	 * Main method of the Hangman game.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// the state show whether to play again
-		boolean again = true;
-		
-		Scanner scan = new Scanner(System.in);
-		// keep playing if the user wants
-		while(again) {
-			// initialize a new hangman game
-			HangmanGame newGame = new HangmanGame();
-			
-			String path = "/Users/xueqiwang/OneDrive - PennO365/UPenn/2nd semester/CIT590/Homework/HW9/words.txt";
-			
-			// traditional hangman mode
-			if(newGame.mode == true)
-				newGame.traditionalHangmanGame(scan, path);
-			// evil hangman mode
-			else
-				newGame.evilHangmanGame(scan, path);
-			
-			// ask the user whether to play again
-			again = newGame.playAgain(scan);
-		}
-		
-		scan.close();
-	}
-	
-	/**
-	 * This method is a new traditional hangman game.
-	 * The method contains the whole game.
-	 * @param scan
-	 * @param path of the txt file
-	 */
-	void traditionalHangmanGame(Scanner scan, String path) {
-		TraditionalHangman newTraditional = new TraditionalHangman(path);
-		
-		String ch;     // user's input
-		boolean valid; // whether the user's input is correct
-		
-		boolean over = false;
-		while(over == false)
-		{
-			// get the user's guess
-			ch = newTraditional.getUserGuess(scan);
-			
-			// check the guess
-			valid = newTraditional.isCorrect(ch);
-			
-			// print incorrect guesses if the current guess is incorrect
-			if(valid == false)
-				newTraditional.incorrectGuesses(ch);
-			
-			// show current known word
-			newTraditional.printKnownWord();
-			
-			// check whether the game is over
-			over = newTraditional.isGameOver();
-		}
-		
-		// show the results
-		newTraditional.showFinalResults();
-		
-	}
-	
-	/**
-	 * This method is a new evil hangman game.
-	 * The method contains the whole game.
-	 * @param scan
-	 * @parat path of txt file
-	 */
-	void evilHangmanGame(Scanner scan, String path) {
-		EvilHangman newEvil = new EvilHangman(path);
-		
-		String ch;     // user's input
-		boolean valid; // whether the user's input is correct
-		boolean over = false;
-		while(over == false)
-		{
-			// get the user's guess
-			ch = newEvil.getUserGuess(scan);
-			
-			// get the new words family
-			
-			newEvil.getWordsFamily(ch);
-			
-			
-			System.out.println(newEvil.wordsFamily);
-			
-			// check the guess
-			valid = newEvil.isCorrect(ch);
-			
-			// print incorrect guesses if the current guess is incorrect
-			if(valid == false)
-				newEvil.incorrectGuesses(ch);
-			
-			// show current known word
-			newEvil.printKnownWord();
-			
-			// check whether the game is over
-			over = newEvil.isGameOver();
-		}
-		
-		
-		// show the results
-		newEvil.showFinalResults();
-	}
-	
-	/**
-	 * This method returns true if the user wants to play again.
-	 * @param scan
-	 * @return again (true/false)
-	 */
-	boolean playAgain(Scanner scan) {
-		
-		boolean valid = false;
-		boolean again = false;
-		
-		while(valid == false) {
-			System.out.println("Do you want to play again? y/n");
-			String ans = scan.next();
-			if(ans.startsWith("y") || ans.startsWith("Y")) {
-				valid = true;
-				again = true;
-			}
-			else if(ans.startsWith("n") || ans.startsWith("N")) {
-				valid = true;
-				again = false;
-			}
-		}
-		return again;
-	}
-	
-	
-}
-
 
 /**
  * This class describes a general word guess game hangman.
@@ -178,6 +18,11 @@ public abstract class Hangman {
 	 * "cleanWords": a String list of available words.
 	 */
 	public List<String> cleanWords = new ArrayList<>();
+	
+	/**
+	 * "word": a randomly selected word for user to guess.
+	 */
+	public String word;
 	
 	/**
 	 * "guesses": the times user guesses.
@@ -200,6 +45,11 @@ public abstract class Hangman {
 	public boolean[] correct;
 	
 	/**
+	 * "incorrectGuesses": a String list that stores all the incorrect guesses.
+	 */
+	public List<String> incorrectGuesses = new ArrayList<>();
+	
+	/**
 	 * This constructor initializes a hangman game and the words list.
 	 * @param path of the txt file
 	 */
@@ -210,9 +60,40 @@ public abstract class Hangman {
 		
 		// set the counters
 		this.guesses = 0;
+		
+		// get the random word
+		this.word = this.getRandomWord();
+		
+		// set the known word to "_____"
+		this.knownWord = new String[this.word.length()];
+		for(int i=0; i<this.getWordLength(); i++)
+			this.knownWord[i] = "_";
+		
+		// print the original known word
+		this.printKnownWord();
+		
+		// initialize the correct array
+		this.correct = new boolean[this.word.length()];
 	}
 	
 	// Method
+	
+	/**
+	 * This method randomly select a word from CleanWords list.
+	 * @return RandomWord (String)
+	 */
+	String getRandomWord() {
+		
+		// get a random index
+		Random r = new Random();
+		int index = r.nextInt(this.cleanWords.size());
+		
+		// find the corresponding word
+		String RandomWord = this.cleanWords.get(index);
+		
+		return RandomWord;
+	}
+	
 	/**
 	 * This method reads the file and get the clean words.
 	 * @param path
@@ -279,10 +160,20 @@ public abstract class Hangman {
 	abstract String getGameType();
 	
 	/**
-	 * This method returns the length of word.
-	 * @return length (int)
+	 * This method returns true if the user's guess is correct.
+	 * @param ch
+	 * @return true/false
 	 */
-	abstract int getWordLength();
+	abstract boolean isCorrect(String ch);
+	
+	/**
+	 * This method returns the length of the word.
+	 * @return length
+	 */
+	int getWordLength() {
+		int length = this.word.length();
+		return length;
+	}
 	
 	// helper
 	/**
@@ -305,7 +196,7 @@ public abstract class Hangman {
 	 */
 	void printKnownWord() {
 		for(int i=0; i<this.knownWord.length; i++)
-			System.out.print(this.knownWord[i]);
+			System.out.print(this.knownWord[i]+" ");
 		System.out.print("\n");
 	}
 	
@@ -332,6 +223,25 @@ public abstract class Hangman {
 		this.guesses += 1;
 				
 		return ch;
+	}
+	
+	/**
+	 * This method shows all the incorrect guesses and add 
+	 * last incorrect guess to the incorrectGuesses array.
+	 * @param ch
+	 */
+	void incorrectGuesses(String ch) {
+		
+		// store the incorrect guess
+		this.incorrectGuesses.add(ch);
+		
+		// show all the incorrect guesses
+		System.out.print("Incorrect Guesses: ");
+		for(int i=0; i<this.incorrectGuesses.size(); i++)
+			System.out.print(this.incorrectGuesses.get(i) + " ");
+		
+		System.out.print("\n");
+		
 	}
 	
 	/**
